df33c592e4151e8ae196e08269a81aea09f6c393
Dev-webp <vjcbangalore@gmail.com>
2025-07-26 16:48:54 +0530

    added

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET") or "fallback-secret-key"
-CORS(app, supports_credentials=True,origins=["https://postgres-frontend-attendance-nuthan472s-projects.vercel.app"])
+CORS(app, supports_credentials=True, origins=[
+    "https://postgres-frontend-attendance.vercel.app",
+    "https://postgres-frontend-attendance-nuthan472.vercel.app"
+])
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
