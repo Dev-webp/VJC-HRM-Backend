02dbffb6eb064e6242f76e8b0be6e84bd73e91d0
Dev-webp <vjcbangalore@gmail.com>
2025-07-26 16:14:36 +0530

    added

--- app.py
+++ app.py
@@ -180,4 +180,6 @@ def update_profile_image():
     conn.close()
     return jsonify({"message": "Image updated"}), 200
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
